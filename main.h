6f685f0b21e0032ba2c43dd7aaf7d45118e00d92
rhid07 <opeyemidurodola007@gmail.com>
2023-08-21 14:05:32 +0100

    main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limit.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+#endif
