5aff29a9e3ad5db104e01ac2346c76a783d73de5
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-22 06:53:12 -0700

    handle_integers

--- main.h
+++ main.h
@@ -7,9 +7,14 @@
 #include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stddef.h>
 
 int _print(const char *str);
 int _print_string(const char *str);
 int _printf(const char *format, ...);
 int _write_character(char c);
+int print_char(char c);
+int print_string(const char *str);
+int print_int(int num);
+
 #endif
